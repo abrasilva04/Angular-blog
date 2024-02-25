cc22f16842c5a77567a804d401744bdbdc2feffa
abrasilva04 <abraaaosilvaalves@gmail.com>
2024-02-25 18:01:09 -0300

    components createds with style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuCardComponent } from './components/menu-card/menu-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BigCardComponent,
+    MenuCardComponent,
+    MenuTitleComponent,
+    SmallCardComponent,
+    HomeComponent,
+   
   ],
   imports: [
     BrowserModule,
